bb5927d538619e3836c0a207bfddbcd30f5616e1
Mathieu Ducrot <mathieu.ducrot@smartbooster.io>
2021-07-05 16:16:45 +0200

    #2 Add ParameterAdmin

---
+++ src/Admin/ParameterAdmin.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Smart\ParameterBundle\Admin;
+
+use Smart\SonataBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+/**
+ * @author Mathieu Ducrot <mathieu.ducrot@smartbooster.io>
+ */
+class ParameterAdmin extends AbstractAdmin
+{
+    protected function configureRoutes(RouteCollection $collection): void
+    {
+        $collection
+            ->remove('create')
+            ->remove('delete')
+        ;
+        parent::configureRoutes($collection);
+    }
+
+    protected function configureListFields(ListMapper $list): void
+    {
+        $list
+            ->addIdentifier('id', null, ['label' => 'field.label_id'])
+            ->addIdentifier('code', null, ['label' => 'field.label_code'])
+            ->add('help', null, ['label' => 'field.label_help'])
+            ->add('value', null, ['label' => 'field.label_value'])
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $filter): void
+    {
+        $filter
+            ->add('code', null, [
+                'show_filter' => true,
+                'label' => 'field.label_code'
+            ])
+        ;
+    }
+
+    protected function configureShowFields(ShowMapper $show): void
+    {
+        $show
+            ->with('fieldset.label_general')
+                ->add('id', null, ['label' => 'field.label_id'])
+                ->add('code', null, ['label' => 'field.label_code'])
+                ->add('help', null, ['label' => 'field.label_help'])
+                ->add('value', null, ['label' => 'field.label_value'])
+            ->end()
+        ;
+    }
+
+    protected function configureFormFields(FormMapper $form): void
+    {
+        $form
+            ->with('fieldset.label_general')
+                ->add('code', null, [
+                    'label' => 'field.label_code',
+                    'disabled' => true
+                ])
+                ->add('help', null, [
+                    'label' => 'field.label_help',
+                    'disabled' => true
+                ])
+                ->add('value', null, ['label' => 'field.label_value'])
+            ->end()
+        ;
+    }
+}
